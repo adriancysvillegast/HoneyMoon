d122ab8dc1de6b30ca4e26c440535eef796f9c22
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-11-14 16:24:12 -0400

    Initial Commit

---
+++ Honey Moon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Honey Moon
+//
+//  Created by Adriancys Jesus Villegas Toro on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Honey Moon/Honey_MoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Honey_MoonApp.swift
+//  Honey Moon
+//
+//  Created by Adriancys Jesus Villegas Toro on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Honey_MoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
